f6de26ad7190ed04556590728c60d226779446d2
Sansriti Sharma <sansritisharma004@gmail.com>
2024-04-08 14:08:11 +0530

    Version 1.0.0 ~ Sansriti Sharma

--- lib/Screens/LoginScreen.dart
+++ lib/Screens/LoginScreen.dart
@@ -16,8 +16,12 @@ class _LogInScreenState extends State<LogInScreen> {
     passwordVisible = true;
   }
 
+  var _formKey = GlobalKey<FormState>();
 
 
+
+  final TextEditingController email = TextEditingController();
+  final TextEditingController password = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -25,136 +29,143 @@ class _LogInScreenState extends State<LogInScreen> {
     return Scaffold(
       body: Center(
 
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
 
 
-            // enter email
-            Row(
+              // enter email
+              Row(
 
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 60.0),
-                  child: const Text("Email"),
-                ),
-                const SizedBox(width: 15),
-                SizedBox(
-                  width: 281,
-                  child: TextField(
-                    style: const TextStyle(
-                      fontSize: 12,
-                      color: Color.fromRGBO(191, 207, 255, 1),
-                    ),
-                    textAlign: TextAlign.left,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      filled: true,
-                      hintText: 'john@bookify.com',
-                      hintStyle: const TextStyle(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 60.0),
+                    child: const Text("Email"),
+                  ),
+                  const SizedBox(width: 15),
+                  SizedBox(
+                    width: 281,
+                    child: TextField(
+                      style: const TextStyle(
                         fontSize: 12,
                         color: Color.fromRGBO(191, 207, 255, 1),
                       ),
-                      isDense: true,
-                      contentPadding: const EdgeInsets.all(10.0),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(color: Color.fromRGBO(191, 207, 255, 1),)
+                      textAlign: TextAlign.left,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        filled: true,
+                        hintText: 'john@bookify.com',
+                        hintStyle: const TextStyle(
+                          fontSize: 12,
+                          color: Color.fromRGBO(191, 207, 255, 1),
+                        ),
+                        isDense: true,
+                        contentPadding: const EdgeInsets.all(10.0),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: const BorderSide(color: Color.fromRGBO(191, 207, 255, 1),)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: const BorderSide(color: Color.fromRGBO(191, 207, 255, 1))
+                        ),
+                        fillColor: Colors.white,
                       ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(color: Color.fromRGBO(191, 207, 255, 1))
-                      ),
-                      fillColor: Colors.white,
                     ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 14),
+                ],
+              ),
+              const SizedBox(height: 14),
 
 
 
-            // enter password
-            Row(
+              // enter password
+              Row(
 
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 33.0),
-                  child: const Text("Password"),
-                ),
-                const SizedBox(width: 15),
-                SizedBox(
-                  width: 281,
-                  child: TextField(
-                    style: const TextStyle(
-                      fontSize: 12,
-                      color: Color.fromRGBO(191, 207, 255, 1),
-                    ),
-                    textAlign: TextAlign.left,
-                    keyboardType: TextInputType.visiblePassword,
-                    obscureText: passwordVisible,
-                    decoration: InputDecoration(
-                      filled: true,
-                      hintText: 'Enter Password',
-                      hintStyle: const TextStyle(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 33.0),
+                    child: const Text("Password"),
+                  ),
+                  const SizedBox(width: 15),
+                  SizedBox(
+                    width: 281,
+                    child: TextField(
+                      style: const TextStyle(
                         fontSize: 12,
                         color: Color.fromRGBO(191, 207, 255, 1),
                       ),
-                      suffixIconConstraints: BoxConstraints(maxHeight: 35, minHeight:35 ),
-                      suffixIcon: IconButton(
-                        icon: Icon(passwordVisible
-                            ? Icons.visibility
-                            : Icons.visibility_off),
-                        color: Color.fromRGBO(191, 207, 255, 1),
-                        onPressed: (){
-                          setState(() {
-                            passwordVisible = !passwordVisible;
-                          });
-                        },
+                      textAlign: TextAlign.left,
+                      keyboardType: TextInputType.visiblePassword,
+                      obscureText: passwordVisible,
+                      decoration: InputDecoration(
+                        filled: true,
+                        hintText: 'Enter Password',
+                        hintStyle: const TextStyle(
+                          fontSize: 12,
+                          color: Color.fromRGBO(191, 207, 255, 1),
+                        ),
+                        suffixIconConstraints: BoxConstraints(maxHeight: 35, minHeight:35 ),
+                        suffixIcon: IconButton(
+                          icon: Icon(passwordVisible
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          color: Color.fromRGBO(191, 207, 255, 1),
+                          onPressed: (){
+                            setState(() {
+                              passwordVisible = !passwordVisible;
+                            });
+                          },
+                        ),
+                        alignLabelWithHint: true,
+                        isDense: true,
+                        contentPadding: const EdgeInsets.only(left: 10.0),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: const BorderSide(color: Color.fromRGBO(191, 207, 255, 1),)
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: const BorderSide(color: Color.fromRGBO(191, 207, 255, 1))
+                        ),
+                        fillColor: Colors.white,
                       ),
-                      alignLabelWithHint: true,
-                      isDense: true,
-                      contentPadding: const EdgeInsets.only(left: 10.0),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(color: Color.fromRGBO(191, 207, 255, 1),)
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
-                          borderSide: const BorderSide(color: Color.fromRGBO(191, 207, 255, 1))
-                      ),
-                      fillColor: Colors.white,
                     ),
                   ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Container(
+                width: 70,
+                height: 70,
+                decoration: BoxDecoration(
+                    color: const Color.fromRGBO(0, 0, 179, 1),
+                    borderRadius: BorderRadius.circular(50),
+                    border: Border.all(
+                        width: 2,
+                        color: Color.fromRGBO(191, 207, 255, 10)
+                    )
                 ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Container(
-              width: 70,
-              height: 70,
-              decoration: BoxDecoration(
-                  color: const Color.fromRGBO(0, 0, 179, 1),
-                  borderRadius: BorderRadius.circular(50),
-                  border: Border.all(
-                      width: 2,
-                      color: Color.fromRGBO(191, 207, 255, 10)
-                  )
+                child: ElevatedButton(
+
+                  style: ElevatedButton.styleFrom(
+
+                  ),
+                    onPressed: (){
+
+
+                    },
+                    child: Text('Login', style: TextStyle(fontSize: 14),)
+                ),
               ),
-              child: ElevatedButton(
-                  onPressed: (){
 
+              TextButton(onPressed: (){}, child: Text("Sign Up"))
 
-                  },
-                  child: Text('Login', style: TextStyle(fontSize: 14),)
-              ),
-            ),
-
-            TextButton(onPressed: (){}, child: Text("Sign Up"))
-
-          ],
+            ],
+          ),
         ),
       ),
     );
